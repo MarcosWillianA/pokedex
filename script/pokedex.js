540177cd15f05e94568c340de18da91b957f9dcc
Marcos Willian <marcoswillianl1987@gmail.com>
2024-11-08 09:49:19 -0300

    Javascript
    Javascript básico, criei as constantes

---
+++ script/pokedex.js
@@ -0,0 +1,31 @@
+const pesquisar = document.querySelector('#pesquisar');
+const botaoPesquisar = document.querySelector('#botao-pesquisar');
+const botaoAnterior = document.querySelector('#botao-anterior');
+const botaoProximo = document.querySelector('#botao-proximo');
+const pokemonInfo = document.querySelector('#pokemon-info');
+const botoesNavegacao = document.querySelector('#botoes-navegacao');
+const pokemonImagem = document.querySelector('pokemon-imagem');
+const pokemonNome = document.querySelector('#pokemon-nome');
+const pokemonDescricao = document.querySelector('#pokemonDescricao');
+const mensagemErro = document.querySelector('#mensagem-erro');
+const mensagemLoading = document.querySelector('#mensagem-loading');
+
+let idPokemonAtual = 1;
+
+const fetchPokemon = async (identificadorPokemon) => {
+    exibirLoading();
+    try {
+        
+    } catch (error) {
+        
+    } finally {
+
+    }
+} 
+
+const exibirLoading = () => {
+    mensagemLoading.classList.remove('esconder');
+    pokemonInfo.classList.add('esconder');
+    mensagemErro.classList.add('erro');
+    botoesNavegacao.classList.add('erro');
+}
